4396b8a1d83e8e3230bca96479a7a0d00e931e93
Sherilyn Maestre <you@example.com>
2022-06-08 20:36:40 -0500

    working on dto package, added ProviderDTO

---
+++ src/main/java/com/sofkau/store/dto/ProviderDTO.java
@@ -0,0 +1,12 @@
+package com.sofkau.store.dto;
+
+import lombok.Data;
+
+@Data
+public class ProviderDTO {
+    private String id;
+    private String name;
+    private String passport;
+    private Integer phone;
+
+}
