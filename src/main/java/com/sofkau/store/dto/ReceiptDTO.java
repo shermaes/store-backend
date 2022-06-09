37f66fa489e1689ed9fe437005e6427cd9143790
Sherilyn Maestre <you@example.com>
2022-06-08 20:37:49 -0500

    working on dto package, added ReceiptDTO

---
+++ src/main/java/com/sofkau/store/dto/ReceiptDTO.java
@@ -0,0 +1,15 @@
+package com.sofkau.store.dto;
+
+import com.sofkau.store.collection.Provider;
+import lombok.Data;
+
+@Data
+public class ReceiptDTO {
+    private String id;
+
+    private Provider provider;
+
+    private String id_product;
+
+    private String date;
+}
