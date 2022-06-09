15b91e422d1478d4620ceec9c7b9917aa4b5fc75
Sherilyn Maestre <you@example.com>
2022-06-08 21:03:06 -0500

    working on router package, added getProviderRoute

--- src/main/java/com/sofkau/store/router/GetProviderRoute.java
+++ src/main/java/com/sofkau/store/router/GetProviderRoute.java
@@ -1,4 +1,26 @@
 package com.sofkau.store.router;
 
+import com.sofkau.store.dto.ProviderDTO;
+import com.sofkau.store.usecase.GetProviderUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class GetProviderRoute {
+    @Bean
+    public RouterFunction<ServerResponse> allCategories(GetProviderUseCase get) {
+        return route(GET("/v1/api/categories"),
+                request -> ServerResponse.status(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromProducer(get.apply(), ProviderDTO.class))
+        );
+    }
 }
