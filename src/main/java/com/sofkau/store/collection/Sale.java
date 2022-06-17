785da4f6f8a0bf16c1208d70f840b697aafd314d
Sherilyn Maestre <you@example.com>
2022-06-17 13:58:11 -0500

    added the update product use case

---
+++ src/main/java/com/sofkau/store/collection/Sale.java
@@ -0,0 +1,41 @@
+package com.sofkau.store.collection;
+
+public class Sale {
+    //here I'll have the products that are being sold so I can generate the bill without teh need to use the product collection
+    private String id;
+    private String product_name;
+    private String price;
+    private String quantity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/sofkau/store/usecase/UpdateProductUseCase.java
@@ -0,0 +1,23 @@
+package com.sofkau.store.usecase;
+
+import com.sofkau.store.dto.ProductDTO;
+import com.sofkau.store.mapper.StoreMapper;
+import com.sofkau.store.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
+public class UpdateProductUseCase {
+    @Autowired
+    private IProductRepository iProductRepository;
+    @Autowired
+    private StoreMapper storeMapper;
+
+    public Mono<ProductDTO> apply(ProductDTO dto){
+        return iProductRepository
+                .save(storeMapper.toEntity(dto))
+                .map(storeMapper::toProductDTO);}
+}
